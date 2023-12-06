b07b111d4c6b065db851a27b6c1d06c5c6d454f5
artem <artem.moskalenko.00@gmail.com>
2023-12-06 15:23:34 +0200

    Serializers: Explicit is better than implicit

--- book_reading/serializers.py
+++ book_reading/serializers.py
@@ -6,10 +6,12 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'title', 'author', 'year_published',
+                  'short_description', 'full_description']
 
 
 class BookWithoutFullDescriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        exclude = ['full_description']
+        fields = ['id', 'title', 'author', 'year_published',
+                  'short_description']
