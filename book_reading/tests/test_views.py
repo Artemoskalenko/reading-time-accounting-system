5974d60e4c05a2199df38e352654d5daffe4a795
artem <artem.moskalenko.00@gmail.com>
2023-12-06 16:57:54 +0200

    Testing changed: Fixtures added, each test checks one small function/url

--- book_reading/tests/test_views.py
+++ book_reading/tests/test_views.py
@@ -12,117 +12,137 @@ from book_reading.services import timedelta_to_string
 from book_reading.tasks import daily_collection_of_user_statistics
 
 
-BOOK_DATA = {
-    'title': 'test_title',
-    'author': 'test_author',
-    'year_published': 2023,
-    'short_description': 'test_short_description',
-    'full_description': 'test_full_description'}
-
-SECOND_BOOK_DATA = {
-    'title': 'test_title2',
-    'author': 'test_author2',
-    'year_published': 2022,
-    'short_description': 'test_short_description2',
-    'full_description': 'test_full_description2'
-}
+@pytest.fixture
+def create_book_1():
+    """Creates the first book"""
+    book_data = {'title': 'test_title',
+                 'author': 'test_author',
+                 'year_published': 2023,
+                 'short_description': 'test_short_description',
+                 'full_description': 'test_full_description'}
+    book = Book.objects.create(**book_data)
+    return book
 
 
-@pytest.mark.django_db
-def test_book_api():
-    client = APIClient()
-
-    response = client.get("/api/v1/books/")
-    assert response.status_code == 200
-    assert len(response.data) == 0
-
-    book = Book.objects.create(**BOOK_DATA)
-
-    response = client.get("/api/v1/books/")
-    assert response.status_code == 200
-    assert len(response.data) == 1
-
-    book_id = response.data[0]["id"]
-
-    response = client.get(f"/api/v1/book-details/{book_id}/")
-    assert response.status_code == 200
-    assert response.data["title"] == book.title
+@pytest.fixture
+def create_book_2():
+    """Creates the second book"""
+    book_data = {'title': 'test_title2',
+                 'author': 'test_author2',
+                 'year_published': 2022,
+                 'short_description': 'test_short_description2',
+                 'full_description': 'test_full_description2'}
+    book = Book.objects.create(**book_data)
+    return book
 
 
-@pytest.mark.django_db
-def test_reading_statistics_api():
-    client = APIClient()
+@pytest.fixture
+def api_client():
+    """Returns the APIClient for making requests"""
+    return APIClient()
 
-    client.post("/auth/users/", {"username": "testusername", "password": "testpassword"})
-    response = client.post("/auth/token/login/", {"username": "testusername", "password": "testpassword"})
-    client.credentials(HTTP_AUTHORIZATION=f"Token {response.data['auth_token']}")
 
-    book = Book.objects.create(**BOOK_DATA)
-    book2 = Book.objects.create(**SECOND_BOOK_DATA)
-    book_serialized = BookWithoutFullDescriptionSerializer(book)
+@pytest.fixture
+def test_user(api_client):
+    """Creates a user by token"""
+    api_client.post("/auth/users/", {"username": "testusername", "password": "testpassword"})
+    response = api_client.post("/auth/token/login/", {"username": "testusername", "password": "testpassword"})
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {response.data['auth_token']}")
 
-    response = client.get(f"/api/v1/book-reading-statistics/{book2.id + 1}/")
-    assert response.status_code == 200
-    assert response.data["Error"] == "There is no book with this ID"
 
-    response = client.get(f"/api/v1/book-reading-statistics/{book.id}/")
-    assert response.status_code == 200
-    assert response.data["Book"] == book_serialized.data
-    assert response.data["Total reading time"] == timedelta_to_string(datetime.timedelta())
-
-    response = client.get(f"/api/v1/start-reading-session/{book2.id + 1}/")
-    assert response.status_code == 200
-    assert response.data["Error"] == "There is no book with this ID"
-
-    response = client.get(f"/api/v1/start-reading-session/{book.id}/")
-    assert response.status_code == 200
-    assert response.data["message"] == "Book reading session started successfully"
-
-    response = client.get(f"/api/v1/start-reading-session/{book.id}/")
-    assert response.status_code == 200
-    assert response.data["message"] == "A reading session for this book is already active"
-
-    response = client.get(f"/api/v1/start-reading-session/{book2.id}/")
-    assert response.status_code == 200
-    assert response.data["message"] == 'The previous book reading session was ended successfully, ' \
-                                       'and the new book reading session started successfully'
-
-    response = client.get(f"/api/v1/end-reading-session/")
-    assert response.status_code == 200
-    assert response.data["message"] == "Book reading session ended successfully"
-
-    response = client.get(f"/api/v1/end-reading-session/")
-    assert response.status_code == 200
-    assert response.data["message"] == "There is currently no book reading session started"
+@pytest.fixture
+def start_reading_session(api_client, create_book_1):
+    """Begins the process of reading a book"""
+    api_client.get(f"/api/v1/start-reading-session/{create_book_1.id}/")
 
 
 @pytest.mark.django_db
-def test_user_reading_statistics_api():
-    client = APIClient()
+class TestViews:
+    def test_books_url(self, api_client, create_book_1):
+        response = api_client.get("/api/v1/books/")
+        assert response.status_code == 200
+        assert len(response.data) == 1
 
-    client.post("/auth/users/", {"username": "testusername", "password": "testpassword"})
-    response = client.post("/auth/token/login/", {"username": "testusername", "password": "testpassword"})
-    client.credentials(HTTP_AUTHORIZATION=f"Token {response.data['auth_token']}")
+    def test_book_details_url(self, api_client, create_book_1):
+        response = api_client.get(f"/api/v1/book-details/{create_book_1.id}/")
+        assert response.status_code == 200
+        assert response.data["title"] == 'test_title'
+        assert response.data["author"] == 'test_author'
+        assert response.data["year_published"] == 2023
+        assert response.data["short_description"] == 'test_short_description'
+        assert response.data["full_description"] == 'test_full_description'
 
-    book = Book.objects.create(**BOOK_DATA)
+    def test_book_reading_statistics_invalid_book_id(self, create_book_1, create_book_2, api_client, test_user):
+        response = api_client.get(f"/api/v1/book-reading-statistics/{create_book_2.id + 1}/")
+        assert response.status_code == 200
+        assert response.data["Error"] == "There is no book with this ID"
 
-    daily_collection_of_user_statistics()
+    def test_book_reading_statistics_with_zero_total_reading_time(self, create_book_1, create_book_2, api_client, test_user):
+        book_serialized = BookWithoutFullDescriptionSerializer(create_book_1)
 
-    response = client.get(f"/api/v1/user-statistics/")
-    assert response.status_code == 200
-    assert response.data["Total reading time"] == timedelta_to_string(datetime.timedelta())
-    assert response.data["Last 7 days reading time"] == timedelta_to_string(datetime.timedelta())
-    assert response.data["Last 30 days reading time"] == timedelta_to_string(datetime.timedelta())
+        response = api_client.get(f"/api/v1/book-reading-statistics/{create_book_1.id}/")
+        assert response.status_code == 200
+        assert response.data["Book"] == book_serialized.data
+        assert response.data["Total reading time"] == timedelta_to_string(datetime.timedelta())
 
-    mocked = datetime.datetime.now() - datetime.timedelta(hours=2)
-    with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
-        client.get(f"/api/v1/start-reading-session/{book.id}/")
-    client.get(f"/api/v1/end-reading-session/")
+    def test_start_reading_session_invalid_book_id(self, create_book_1, create_book_2, api_client, test_user):
+        response = api_client.get(f"/api/v1/start-reading-session/{create_book_2.id + 1}/")
+        assert response.status_code == 200
+        assert response.data["Error"] == "There is no book with this ID"
 
-    daily_collection_of_user_statistics()
+    def test_start_reading_session(self, create_book_1, create_book_2, api_client, test_user):
+        response = api_client.get(f"/api/v1/start-reading-session/{create_book_1.id}/")
+        assert response.status_code == 200
+        assert response.data["message"] == "Book reading session started successfully"
 
-    response = client.get(f"/api/v1/user-statistics/")
-    assert response.status_code == 200
-    assert response.data["Total reading time"] == timedelta_to_string(datetime.timedelta(hours=2))
-    assert response.data["Last 7 days reading time"] == timedelta_to_string(datetime.timedelta(hours=2))
-    assert response.data["Last 30 days reading time"] == timedelta_to_string(datetime.timedelta(hours=2))
+    def test_start_reading_session_already_active(self, create_book_1, create_book_2,
+                                                  api_client, test_user, start_reading_session):
+        response = api_client.get(f"/api/v1/start-reading-session/{create_book_1.id}/")
+        assert response.status_code == 200
+        assert response.data["message"] == "A reading session for this book is already active"
+
+    def test_start_reading_session_with_active_session(self, create_book_1, create_book_2,
+                                                       api_client, test_user, start_reading_session):
+        response = api_client.get(f"/api/v1/start-reading-session/{create_book_2.id}/")
+        assert response.status_code == 200
+        assert response.data["message"] == 'The previous book reading session was ended successfully, ' \
+                                           'and the new book reading session started successfully'
+
+    def test_end_reading_session_with_active_session(self, create_book_1, create_book_2,
+                                                     api_client, test_user, start_reading_session):
+        response = api_client.get(f"/api/v1/end-reading-session/")
+        assert response.status_code == 200
+        assert response.data["message"] == "Book reading session ended successfully"
+
+    def test_end_reading_session_without_active_session(self, create_book_1, create_book_2,
+                                                        api_client, test_user):
+        response = api_client.get(f"/api/v1/end-reading-session/")
+        assert response.status_code == 200
+        assert response.data["message"] == "There is currently no book reading session started"
+
+    def test_user_reading_statistics_with_zero_total_reading_time(self, api_client, create_book_1, test_user):
+        """Testing general user statistics without reading the book before"""
+        daily_collection_of_user_statistics()
+
+        response = api_client.get(f"/api/v1/user-statistics/")
+        assert response.status_code == 200
+        assert response.data["Total reading time"] == timedelta_to_string(datetime.timedelta())
+        assert response.data["Last 7 days reading time"] == timedelta_to_string(datetime.timedelta())
+        assert response.data["Last 30 days reading time"] == timedelta_to_string(datetime.timedelta())
+
+    def test_user_reading_statistics(self, api_client, create_book_1, test_user):
+        """Testing general user statistics after two hours of reading a book"""
+
+        # Simulate reading a book for two hours
+        mocked = datetime.datetime.now() - datetime.timedelta(hours=2)
+        with mock.patch('django.utils.timezone.now', mock.Mock(return_value=mocked)):
+            api_client.get(f"/api/v1/start-reading-session/{create_book_1.id}/")
+        api_client.get(f"/api/v1/end-reading-session/")
+
+        daily_collection_of_user_statistics()
+
+        response = api_client.get(f"/api/v1/user-statistics/")
+        assert response.status_code == 200
+        assert response.data["Total reading time"] == timedelta_to_string(datetime.timedelta(hours=2))
+        assert response.data["Last 7 days reading time"] == timedelta_to_string(datetime.timedelta(hours=2))
+        assert response.data["Last 30 days reading time"] == timedelta_to_string(datetime.timedelta(hours=2))
